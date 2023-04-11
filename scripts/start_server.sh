1f24263ad04982f6cf83e3fc5c6a3a818b6a9575
nuomizai <769572294@qq.com>
2023-04-11 19:41:41 +0800

    update

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-export CARLA_ROOT=/home/quan/carla_0.9.10
-#export CARLA_ROOT=/home/liuchi/zhaoyinuo/carla_0.9.10
+export CARLA_ROOT=/home/quan/carla_0.9.10 # [PATH TO YOUR LOCAL DIRECTORY WITH CaraUE4.sh]
 
 SDL_HINT_CUDA_DEVICE=1 ${CARLA_ROOT}/CarlaUE4.sh -carla-world-port=8010 -quality-level=Epic -resx=800 -resy=600 -opengl
 #sleep 5
